8d40a40e627e29a849c692f845cc62193b91aa40
Vaibhav <vaibhavmishra72004@gmail.com>
2023-08-18 18:03:18 +0530

    another commit

---
+++ Javascript/script.js
@@ -0,0 +1,73 @@
+score = 0;
+cross = true;
+
+audio = new Audio('../Resource/music1.mp3');
+audiogo = new Audio('../Resource/gameover.mp3');
+
+setTimeout(() => {
+    audio.play()
+}, 100);
+
+document.onkeydown = function (e) {
+    console.log("Key code is: ", e.keyCode)
+    if (e.keyCode == 38 || e.keyCode == 87 ) {
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(() => {
+            dino.classList.remove('animateDino')
+        }, 700);
+    }
+    if (e.keyCode == 39 || e.keyCode == 68) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+    if (e.keyCode == 37 || e.keyCode == 65) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112) + "px";
+    }
+}
+
+setInterval(() => {
+    dino = document.querySelector('.dino');
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacle');
+
+    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
+
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
+
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+
+    if (offsetX < 160 && offsetY < 52) {
+        gameOver.innerHTML = "Game Over - Reload to Play Again ";
+        obstacle.classList.remove('obstacleAni')
+        audiogo.play();
+        setTimeout(() => {
+            audiogo.pause();
+            audio.pause();
+        }, 1000);
+    }
+    
+    else if (offsetX < 52 && cross) {
+        score += 1;
+        updateScore(score);
+        cross = false;
+        setTimeout(() => {
+            cross = true;
+        }, 1000);
+        setTimeout(() => {
+            aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.01;
+            obstacle.style.animationDuration = newDur + 's';
+        }, 500);
+    }
+}, 10);
+
+function updateScore(score) {
+    scoreCont.innerHTML = "Your score: " + score;
+}
